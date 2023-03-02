6e1a7b8c2bf27c4dd70c6b1bd37637cc07d3ae2d
Darina Y <darina.yarkaeva@gmail.com>
2023-03-02 21:25:46 +0500

    Create project for pracrical task

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(),
+    );
+  }
+}
